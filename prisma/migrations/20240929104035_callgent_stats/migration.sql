23a252bd97d008d3bc0155356c6baab3a4f840c3
dev-callgent <dev@callgent.com>
2024-09-29 19:35:07 +0800

    feat: callgent stats
    Signed-off-by: dev-callgent <dev@callgent.com>

--- prisma/migrations/20240929104035_callgent_stats/migration.sql
+++ prisma/migrations/20240929104035_callgent_stats/migration.sql
@@ -1,31 +1,8 @@
 -- AlterTable
 ALTER TABLE "Callgent" ADD COLUMN     "avatar" VARCHAR(1023),
-ADD COLUMN     "favorited" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "favorite" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "forked" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "liked" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "viewed" INTEGER NOT NULL DEFAULT 0,
-ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "CallgentFunction" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "CallgentRealm" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "Endpoint" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "EventListener" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "Task" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "TaskAction" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
-
--- AlterTable
-ALTER TABLE "UserIdentity" ALTER COLUMN "tenantPk" SET DEFAULT (current_setting('tenancy.tenantPk')::int);
+ADD COLUMN     "featured" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "official" BOOLEAN NOT NULL DEFAULT false;
